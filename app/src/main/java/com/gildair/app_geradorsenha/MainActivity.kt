0e38792611debbe0f88a4542cf7bc998d311254e
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-10-26 20:50:15 -0300

    feat: adicionando logica de gerar a senha e exibi-la

--- app/src/main/java/com/gildair/app_geradorsenha/MainActivity.kt
+++ app/src/main/java/com/gildair/app_geradorsenha/MainActivity.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.gildair.app_geradorsenha.databinding.ActivityMainBinding
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val alfabeto = arrayOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btGerarSenha
+            .setOnClickListener {
+                gerarSenha()
+            }
+    }
+    private fun gerarSenha() {
+        val primeiraLetra = (alfabeto.indices).random()
+        val segundaLetra = (alfabeto.indices).random()
+
+        val letraAleatoria1 = alfabeto[primeiraLetra]
+        val letraAleatoria2 = alfabeto[segundaLetra]
+
+        val numeroAleatorio = Random.nextInt(1000,9999)
+
+
+        binding.txtSenha.text = "$letraAleatoria1$letraAleatoria2 - $numeroAleatorio"
     }
 }
